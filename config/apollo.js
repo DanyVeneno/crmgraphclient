1328e4a6b87224e6b0b504c2453f6371fa620d02
daniel <danieledjv@gmail.com>
2023-06-18 17:47:21 -0600

    fixing fetch2

--- config/apollo.js
+++ config/apollo.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "https://majestic-fire.up.railway.app",
+  uri: "https://crmgrapghql.vercel.app/",
   fetch
 });
 
